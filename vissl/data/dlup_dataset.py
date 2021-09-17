355e896c3afb0627121719696781774cc0181dcc
YoniSchirris <yschirris@gmail.com>
2021-09-17 12:02:27 +0200

    adjust dlup_dataset to take a path to a .txt file holding relative wsi file paths, and a root dir from the config file, and that creates a single dataset out of all wsis

--- vissl/data/dlup_dataset.py
+++ vissl/data/dlup_dataset.py
@@ -3,10 +3,16 @@
 
 import numpy as np
 
+from typing import List
+
+from hissl.utils.io import txt_of_paths_to_list
+
+import pathlib
+
 import PIL.Image
 
 try:
-    from dlup.data.dataset import TiledROIsSlideImageDataset
+    from dlup.data.dataset import TiledROIsSlideImageDataset, ConcatDataset
     from dlup.tiling import TilingMode
 except:
     raise ImportError(
@@ -78,10 +84,22 @@ class DLUPSlideImageDataset:
         ----------
         cfg :
             The complete run configuration from the config.yaml file
+        Required cfg parameters :
+            DATA.DLUP.MPP: float = mpp as defined in `SlideImageDataset`
+            DATA.DLUP.TILE_SIZE.X: int = tile_size as defined in `SlideImageDataset`
+            DATA.DLUP.TILE_SIZE.Y: int = tile_size as defined in `SlideImageDataset`
+            DATA.DLUP.TILE_OVERLAP.X: int = overlap as defined in `SlideImageDataset`
+            DATA.DLUP.TILE_OVERLAP.Y: int = overlap as defined in `SlideImageDataset`
+            DATA.DLUP.TILE_MODE: str = tile mode as defined in `SlideImageDataset`
+            DATA.DLUP.CROP: bool = crop as defined in `SlideImageDataset`
+            optional, not implemented: DATA.DLUP.MASK: str = .txt file holding paths to masks for each WSI
+            DATA.DLUP.MASK_THRESHOLD: float = threshold for the mask as defined in `SlideImageDataset`
+            DATA.DLUP.ROOT_DIR: str = root dir from which paths in file passed in `path` can be found
         data_source :
             name of the type of datasource. should be dlup_wsi
         path :
-            Path to a single WSI
+            Path to a .txt file with relative paths to WSIs for this set and fold. Uses cfg.DATA.DLUP.ROOT_DIR
+            as root dir to search for relative file paths from
         split :
             TRAIN or TEST or VAL. We set it up so this is unused by HISSL, as the path should point to a .txt file
             holding all paths to the WSIs used for this split and fold
@@ -107,6 +125,10 @@ class DLUPSlideImageDataset:
         tile_mode = TilingMode[cfg["DATA"]["DLUP"].TILE_MODE]
         crop = cfg["DATA"]["DLUP"].CROP
         mask_threshold = cfg["DATA"]["DLUP"].MASK_THRESHOLD
+        root_dir = cfg["DATA"]["DLUP"].ROOT_DIR
+
+        path = pathlib.Path(path)
+        wsi_paths = txt_of_paths_to_list(path)
 
         try:
             mask = self.load_mask(
@@ -120,8 +142,8 @@ class DLUPSlideImageDataset:
         # pathlib objects
         transform = TransformDLUP2HISSL()
 
-        self.dlup_dataset = TiledROIsSlideImageDataset.from_standard_tiling(
-            path,
+        single_wsi_datasets: list = [TiledROIsSlideImageDataset.from_standard_tiling(
+            root_dir/wsi_path,
             mpp,
             tile_size,
             tile_overlap,
@@ -130,12 +152,17 @@ class DLUPSlideImageDataset:
             mask,
             mask_threshold,
             transform,
-        )
+        ) for wsi_path in wsi_paths]
 
-    def load_mask(self, mask: str) -> np.ndarray:
+        self.dlup_dataset = ConcatDataset(single_wsi_datasets)
+
+    def load_mask(self, mask: str) -> List[np.ndarray]:
         """
         Load the binary mask used to filter each region, as used in SlideImageDataset
         """
+        # .txt -> List[pathlib.Path]
+        # List[pathlib.Path] -> List[np.ndarray]
+        # return List[np.ndarray]
         raise NotImplementedError
 
     def num_samples(self) -> int:
