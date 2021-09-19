8da3970806e4c90d9c161e80bac360f77602fda6
sinberlin2 <sjdoyle46@gmail.com>
2021-09-19 12:12:04 +0200

    update vissl to reduce changes to facebook library

--- run_singularity_parallel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p 'Select GPUs to use for parallel training (format example: "1,3,5"): ' GPU_IDX
-SINGULARITYENV_CUDA_VISIBLE_DEVICES="${GPU_IDX}" \
-singularity shell --nv \
---bind /mnt/archive/data/pathology/TCGA/gdc_manifest_all_BRCA_DX-2020-08-05/images/,/home/"$USER"/project/dlup:/dlup,/home/"$USER"/project/vissl/:/vissl,/mnt/archive/projectdata/drop/data:/data,/processing:/processing,/mnt/archive/projectdata/drop/ \
- /mnt/archive/containers/hissl_pytorch181_cu111_v1.sif
